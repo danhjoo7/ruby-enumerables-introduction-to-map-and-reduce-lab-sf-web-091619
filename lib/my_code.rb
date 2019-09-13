1f6bfda27f07f28041cda785d7a3e92ac9cc6d96
Daniel Joo <danhjoo@gmail.com>
2019-09-13 21:09:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,52 @@
-# My Code here....
+def map_to_negativize(source_array)
+  new_array = []
+  
+  source_array.length.times do |index|
+    new_value = source_array[index]
+    new_value*= -1
+    new_array.push(new_value)
+  end
+  new_array
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  new_array = []
+  
+  source_array.length.times do |index|
+    new_value = source_array[index]
+    new_value*= 2
+    new_array.push(new_value)
+  end
+  new_array
+end
+
+def map_to_square(source_array)
+  new_array = []
+  
+  source_array.length.times do |index|
+    new_value = source_array[index]
+    new_value**=2
+    new_array.push(new_value)
+  end
+  new_array
+end
+
+def reduce_to_total(source_array, starting_point)
+  starting_point = source_array[index]
+  
+  if index == 0 
+    source_array[index] 
+  
+  
+
+end
+
+def reduce_to_all_true(source_array)
+end
+
+def reduce_to_any_true(source_array)
+end
